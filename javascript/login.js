1d5b3ad7fa8018cfcf2d54c2f40526fe0c931cc6
jmdada24 <jmdalumpines24@gmail.com>
2025-11-04 15:17:18 +0800

    Enigma Web Dev Templates for Everyone

--- javascript/login.js
+++ javascript/login.js
@@ -1,25 +1,24 @@
  function loginForm(){
 
-            const email = document.getElementById('email').value;
-            const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-            // Your username change it whenever you want bro.
-            const username = "robertotoy";
-
-
-            if(email == "robertdoe@example.com" && password =="123"){
-
-                alert("Login Successfully");
-
-                // put your username for this in this localStorage to display Dashboard page 
-                localStorage.setItem ("username", username);
-                window.location.href = "html/dashboard.html"
-            
-            }else{
-                alert('Invalid Credentials');
-
-            }
+    // Your username change it whenever you want bro.
+    const username = "robertotoy";
 
 
-        }
-       
+    if(email == "robertdoe@example.com" && password =="123"){
+
+        alert("Login Successfully");
+
+        // put your username for this in this localStorage to display Dashboard page 
+        localStorage.setItem ("username", username);
+        window.location.href = "html/dashboard.html"
+    
+    }else{
+        alert('Invalid Credentials');
+
+    }
+
+
+}

e89f421af40401bdb6d00aa2b4a05cbcdcd04f24
jmdada24 <jmdalumpines24@gmail.com>
2025-11-04 15:09:56 +0800

    Enigma Web Dev Templates for Everyone

--- javascript/login.js
+++ javascript/login.js
@@ -2,12 +2,16 @@
 
             const email = document.getElementById('email').value;
             const password = document.getElementById('password').value;
+
+            // Your username change it whenever you want bro.
             const username = "robertotoy";
 
 
             if(email == "robertdoe@example.com" && password =="123"){
-                //alert(email + ' ' + password);
-                alert("Login Successfully")
+
+                alert("Login Successfully");
+
+                // put your username for this in this localStorage to display Dashboard page 
                 localStorage.setItem ("username", username);
                 window.location.href = "html/dashboard.html"
             
